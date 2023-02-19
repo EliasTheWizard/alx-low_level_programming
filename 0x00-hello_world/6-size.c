d42aa1142d55c265d257547ef92577880362c16e
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-19 08:11:51 -0800

    We did it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,4 @@
-#include<stdio.h>"
+#include<stdio.h>
 
 /**
  * main - Entry Point
@@ -12,7 +12,7 @@ int main(void)
 	int Size_int = sizeof(int);
 	
 	int Size_long = sizeof(long int);
-
+	
 	int Size_Long = sizeof(long long int);
 
 	int Size_float = sizeof(float);
