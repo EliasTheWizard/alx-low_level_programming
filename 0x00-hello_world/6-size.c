577fd0f8f92b3671180ae89beb0a9debc7cb2b63
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-19 07:00:46 -0800

    Whats my Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int Size_int=sizeof(int);
+	int Size_float=sizeof(float);
+
+	printf("%d\n",Size_int);
+
+	printf("%d\n",Size_float);
+
+	return (0);
+}
