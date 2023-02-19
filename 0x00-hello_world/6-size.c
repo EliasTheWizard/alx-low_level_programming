e86e9b99ac45a14c3ab14872d91f856c9dc08242
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-19 07:37:24 -0800

    We did it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,21 @@
  */
 int main(void)
 {
-	int Size_int=sizeof(int);
-	int Size_float=sizeof(float);
+	int Size_char = sizeof(char);
+	int Size_int = sizeof(int);
+	int Size_long = sizeof(long int);
+	int Size_Long = sizeof(long long int);
+	int Size_float =sizeof(float);
 
-	printf("%d\n",Size_int);
+	printf("size of a char: %d\n",Size_char);
 
-	printf("%d\n",Size_float);
+	printf("size of an int: %d\n",Size_int);
+	
+	printf("size of a long int: %d\n",Size_long);
+	
+	printf("size of a long long int: %d\n",Size_Long);
+	
+	printf("size of a float %d\n",Size_float);
 
 	return (0);
 }
