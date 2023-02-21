2f5edffeba06afaf540de18f74caada33d228877
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-21 20:51:54 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,13 +4,3 @@
 void print_alphabet(void);
 int _putchar(char);
 void print_char(void);
-/**
- * print_char - Prints out string of characters
- *
- * Return: Always 0 takes in no argument
- */
-
-void print_char()
-{
-	puts("_putchar");
-}
