0ae87e3cab14d36754a829a73fa0ba3c53f2083c
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-21 06:53:34 -0800

    Lets get that money

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<unistd.h>
 
 /**
  * This File contains functions
@@ -7,8 +8,37 @@
  *
  * Return: 0 success
  */
-
 void print_out(void)
 {
 	puts("_putchar");
+}
+
+/**
+ * alpha_low - output characters
+ *
+ * Return: 0 (Success)
+ */
+void alpha_low(char x)
+{
+	write(STDOUT_FILENO, &x, 1);
+}
+
+/**
+ * print_alphabet - Print out alphabet in lowercase
+ *
+ * Return: 0 Success
+ *
+ */
+void print_alphabet()
+{
+        char alpha;
+
+        alpha = 'a';
+
+        while(alpha <= 'z')
+        {
+                alpha_low(alpha);
+
+                alpha++;
+        }
 }
