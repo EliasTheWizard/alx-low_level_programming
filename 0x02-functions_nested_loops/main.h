e5739b42c2fbc61da7fbee7d1e1116bb8c71fcb8
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-21 06:57:02 -0800

    Lets get that money

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,18 +2,6 @@
 #include<unistd.h>
 
 /**
- * This File contains functions
- *
- * print_out : displays characters
- *
- * Return: 0 success
- */
-void print_out(void)
-{
-	puts("_putchar");
-}
-
-/**
  * alpha_low - output characters
  *
  * Return: 0 (Success)
