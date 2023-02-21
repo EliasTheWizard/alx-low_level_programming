0157d01b0577b6b4e973a226f32680c66f6477f7
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-21 20:42:13 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_alphabet - Prints alphabet in lower case
+ *
+ * Return: the alphabet
+ */
+void print_alphabet()
+{
+	char alphabet;
+
+  alphabet = 'a';
+  while (alphabet <= 'z') {
+    _putchar(alphabet);
+    alphabet++;
+  }
+}
+
+/**
  * main - check the code
  *
  * Return: Always 0.
