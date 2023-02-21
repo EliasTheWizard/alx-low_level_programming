e23a03376ca1b90bb20c6ef1c6f0943f697c8dac
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-21 20:30:29 +0200

    Added a new function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include "_putchar.c"
+/**
+ * print_alphabet Protoype
+ */
+void print_alphabet(void);
 
 /**
  * print_char - Prints out string of characters
@@ -9,4 +14,20 @@
 void print_char()
 {
 	puts("_putchar");
+}
+
+/**
+ * print_alphabet - Prints alphabet in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet()
+{
+	char alphabet;
+
+  alphabet = 'a';
+  while (alphabet <= 'z') {
+    _putchar(letter);
+    alphabet++;
+  }
 }
