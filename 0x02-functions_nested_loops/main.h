f782024ca53bda4f21fed211a8a892e7f55725fe
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-21 20:39:22 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,6 +4,7 @@
  * print_alphabet Protoype
  */
 void print_alphabet(void);
+int _putchar(char);
 
 /**
  * print_char - Prints out string of characters
@@ -14,20 +15,4 @@ void print_alphabet(void);
 void print_char()
 {
 	puts("_putchar");
-}
-
-/**
- * print_alphabet - Prints alphabet in lower case
- *
- * Return: Always 0 (Success)
- */
-void print_alphabet()
-{
-	char alphabet;
-
-  alphabet = 'a';
-  while (alphabet <= 'z') {
-    _putchar(alphabet);
-    alphabet++;
-  }
 }
