93baca60b267685150be06ed8cc0f664bdf1225d
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-22 09:06:58 -0800

    We finish today

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,16 @@
 /**
  * _islower - Checks if a character is in lower case or not
  *
- * Return: 1 if character is in lower case else 0
+ * ch: variable to hold ASCII code of input character
+ *
+ * Return: 1 if ch is in lower case else 0
  */
 int _islower(int ch)
 {
-	if (ch >= 97 && ch <= 122)/* 97 and 122 are from the ASCII table representing a-z(lowercase alphabet)*/
+	/** 
+	 * 97 and 122 are ASCII for lowercase characters
+	 */ 
+	if (ch >= 97 && ch <= 122)
 	{
 		return (1);
 	}
