9ae15cbffeceaadf6a1a47f6fab6491579579f3f
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 21:41:51 +0200

    Time Table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,32 @@
+#include"main.h"
 
+/**
+ * times_table - prints out the 9 times table
+ *
+ * Return: 0 on Success
+*/
+
+void times_table(void)
+{
+	int num, multiplier, product;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(48);
+		for (multiplier = 1; multiplier <= 9; multiplier++)
+		{
+			_putchar(',');/* formatting to fit example code*/
+			_putchar(' ');
+
+			prod = num * multiplier;
+
+			if (product <= 9)
+				_putchar(' ');/* formatting to fit example code*/
+			else
+				_putchar((product / 10) + 48);
+
+			_putchar((product % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
