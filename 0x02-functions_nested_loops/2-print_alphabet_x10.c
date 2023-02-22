645d2b5ba619df2af6bf006849b2ee7367883b61
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 16:42:02 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet - Prints alphabet in lower case 10 times
+ * print_alphabet_x10 - Prints alphabet in lower case 10 times
  *
  * Return: the alphabet
  */
