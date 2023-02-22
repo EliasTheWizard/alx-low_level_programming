60dae290e39f2e34f76edaf8fb757ef121c72d2c
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 16:13:51 +0200

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,7 +7,7 @@
  */
 void print_alphabet_x10(void)
 {
-	int alphabet;
+	char alphabet;
 	int count;
 
 	alphabet = 'a';
@@ -17,10 +17,7 @@ void print_alphabet_x10(void)
 		while (alphabet <= 'z')
 		{
 			_putchar(alphabet);
-			++alphabet;
-		}
-		
-		_putchar('\n');
-		
+			alphabet++;
+		}	
 	}
 }
