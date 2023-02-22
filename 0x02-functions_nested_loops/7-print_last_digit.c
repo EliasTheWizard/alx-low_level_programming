6f819cc4b8be00746cb1ae3d6cd32a055a89d515
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 20:58:18 +0200

    Last Num standing

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * print_last_digit - print out the last digit of a number.
+ *
+ * @num: variable to hold number
+ *
+ * Return: The last digit of the number input
+*/
+
+int print_last_digit(int num)
+{
+	int lastDigit;
+
+	if (num < 0)
+		lastDigit = -1 * (num % 10);
+	else
+		lastDigit = num % 10;
+
+	_putchar(lastDigit + '0');
+	return (lastDigit);
+}
