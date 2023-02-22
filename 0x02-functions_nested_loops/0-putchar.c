b4a4e53ac7652bc2bec7505668b4c2cc5616544f
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 14:28:34 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -12,14 +12,12 @@ int main(void)
 	/* keeps the number of letters needed to write the word _putchar*/
 	char Word[] = "_putchar";
 	int W_count = 0;
-	
 	while (W_count < 8)
 	{
 		_putchar(Word[W_count]);
-		
 		W_count++;
 	}
-	
+
 	_putchar('\n');
 
 	return (0);
