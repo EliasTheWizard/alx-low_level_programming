a50091a70448f1f7d901d60733340ee8a9b15c7e
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 20:43:23 +0200

    Show some abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,18 @@
+#include "main.h"
 
+/**
+ * _abs - Calculates and returns the absolute value of input
+ *
+ * @num: variable to hold input
+ *
+ * Return: absolute value of num
+*/
+int _abs(int num)
+{
+  if (num < 0)
+  {
+    num =* -1;
+  }
+  
+  return (num);
+ }
