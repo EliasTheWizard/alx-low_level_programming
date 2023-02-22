1cb9bbe2c28b4f00ba27d76f631b1109deb2f95b
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 20:09:49 +0200

    Who's The Alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _islower - Checks if a character is in lower case or not
+ * _isalpha - Checks if a character is in lower case or not
  *
  * @ch: variable to hold ASCII code of input character
  *
- * Return: 1 if ch is in lower case else 0
+ * Return: 1 if ch is alphabet character else 0
  */
-int _islower(int ch)
+int _isalpha(int ch)
 {
 	/**
 	 * 97-122 or 65-90 are ASCII for Alphabet
