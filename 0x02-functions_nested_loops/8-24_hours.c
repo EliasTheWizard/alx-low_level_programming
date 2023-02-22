b84ed99c37cf76fc41730c4c201885df1f88d4c6
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 21:16:08 +0200

    Agent Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,30 @@
+#include"main.h"
 
+/**
+ * jack_bauer- prints every minute of the day from 00:00 to 23:59
+ *
+ * Return : void AKA 0 
+*/
+
+void jack_bauer(void)
+{
+	int Hr, Min;
+
+	for (Hr = 0; Hr < 24; Hr++)
+	{
+		for (Min = 0; Min < 60; Min++)
+		{
+			_putchar((Hr / 10) + 48);
+      
+			_putchar((Hr % 10) + 48);
+			
+      _putchar(':');
+			
+      _putchar((Min / 10) + 48);
+			
+      _putchar((Min % 10) + 48);
+			
+      _putchar('\n');
+		}
+	}
+}
