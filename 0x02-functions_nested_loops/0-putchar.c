a6beafa3094497e08bfef2aeecd26e80f2d328b5
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 11:21:19 +0200

    0-putchar.c print out characters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- * print_char - Prints out string of characters
- *
- * Return: Always 0 takes in no argument
- */
-
-void print_char()
-{
-	puts("_putchar");
-}
-
-
-/**
  * main - Entry Point
  *
  * Return: Always 0 (success)
