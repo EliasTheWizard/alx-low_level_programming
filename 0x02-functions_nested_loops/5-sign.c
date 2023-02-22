5997a0b059ce2a97e84426d7d14024752967c3de
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 20:24:21 +0200

    Do you see the signs?

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,31 @@
+#include "main.h"
 
+/**
+ * print_sign - print out the sign of a number/input
+ *
+ * @num: variable to hold the number to undergo check.
+ *
+ * Return: 1 if num > 0, 0 if num == 0 and -1 if num < 0
+*/
+
+int print_sign(int num)
+{
+	if (num > 0)
+	{
+		_putchar(43 + 0);
+    
+    return (1);
+	}
+	else if (num == 0)
+	{
+		_putchar(0 + 48);
+		
+    return (0);
+	}
+	else
+	{
+		_putchar(45 + 0);
+		
+    return (-1);
+	}
+}
