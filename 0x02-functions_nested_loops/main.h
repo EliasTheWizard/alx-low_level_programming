531e5e0fbe46c8f6976dea752f8c8eea98c3c2b6
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-22 20:08:44 +0200

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,9 +2,14 @@
 #define MAIN_H
 #include<stdio.h>
 
+/**
+ * Function Declarations for the source codes in REPO
+ */
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
+int _isalpha(int c);
 
 #endif
