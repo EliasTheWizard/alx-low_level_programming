798ee53a042fce3473de008ff69b4fba60d5ca50
EliasTheWizard <mhangomphatso6@gmail.com>
2023-02-22 11:53:49 -0800

    REturn the Sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,2 +1,19 @@
 #include "main.h"
 
+/**
+ * add - accept and add two intergers
+ *
+ * @num1: First number
+ *
+ * @num2: Second number
+ *
+ * Return:the summ of two numbers
+ */
+int add(int num1, int num2)
+{
+	int sum;
+
+	sum = num1 + num2;
+
+	return (sum);
+}
