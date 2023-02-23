bd25f0d154d3c653fb9b52c754cc41b226e4ec6f
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-23 14:08:07 +0200

    Create 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * code to test the function positive_or_negative()
+ *
+ * main - Entry point
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+  int num;
+  
+  num = 0;
+  
+  positive_or_negative(num);
+  
+  return (0);
+}
