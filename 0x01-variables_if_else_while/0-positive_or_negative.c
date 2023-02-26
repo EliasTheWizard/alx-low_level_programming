97911004f7b2e28bee7230dd626ed5fb6c56f63d
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-26 14:07:29 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (n > 0)
 	{
-		puts("%d is postive", n);
+		printf("%d is postive\n", n);
 	}
 	else if (n < 0)
 	{
-		puts("%d is negative", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		puts("%d is zero", n);
+		printf("%d is zero\n", n);
 	}
 
 	return (0);
