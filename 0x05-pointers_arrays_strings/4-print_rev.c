493632d6127027fa69f992fe15ce84139721a9af
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-27 20:34:29 +0200

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,17 +11,17 @@
 void print_rev(char *s)
 {
 	int pos;
-	
+
 	/*get length*/
 	for (pos = 0; s[pos] != '\0'; ++pos)
 	{
 		;
 	}
-	
+
 	for (--pos; pos >= 0; --pos)
 	{
 		_putchar(s[pos]);
 	}
-	
+
 	_putchar('\n');
 }
