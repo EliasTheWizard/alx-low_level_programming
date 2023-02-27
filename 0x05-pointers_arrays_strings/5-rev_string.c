447268e23e4a5bf147618407915f13c281c0b4c0
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-27 20:37:30 +0200

    Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+*/
+
+void rev_string(char *s)
+{
+	int l, i;
+	char ch;
+
+	/*get length*/
+	for (l = 0; s[l] != '\0'; ++l)
+  {
+      ;
+  }
+
+	/*swap code block*/
+	for (i = 0; i < l / 2; ++i)
+	{
+		ch = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = ch;
+	}
+
+}
