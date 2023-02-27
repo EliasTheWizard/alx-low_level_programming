e8327336bdbe4a491095734e0861a6cc1b05dde6
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-27 20:30:43 +0200

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include"main.h"
+
+/**
+ * print_rev - prints input backwards
+ *
+ * @s: Variable to take string
+ *
+ * Return: Void
+*/
+
+void print_rev(char *s)
+{
+	int pos;
+
+  /*get length*/
+	for (pos = 0; s[pos] != '\0'; ++pos)
+  {
+    ;
+  }
+  
+	for (--pos; pos >= 0; --pos)
+  {
+    _putchar(s[pos]);
+  }
+  
+	_putchar('\n');
+}
