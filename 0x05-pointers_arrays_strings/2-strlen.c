3c739ad7cc2503d8aa6e658535d0d8c94a12a379
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-27 20:12:16 +0200

    Created Function to measure string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - Checks the length of a string
+ *
+ * @s: variable to take string input
+ *
+ * Return: length of string
+*/
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; ++s)
+  {
+		++counter;
+  }
+
+	return (counter);
+}
