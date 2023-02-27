d86cdaf5b21800541ae164ef5e07c726fab3d5a5
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-02-27 20:46:13 +0200

    Putin 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include"main.h"
+
+/**
+ * puts2 - print the characters in string given
+ *
+ * @str: Variable to take string
+ *
+ * Return: Void 
+*/
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
