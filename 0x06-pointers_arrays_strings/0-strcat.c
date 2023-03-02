14ed8237999101c9324e598464d09ca6b47fcef0
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-02 01:23:10 -0800

    The concantenator

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,28 @@
+#include "main.h"
 
+/**
+ * _strcat - Concatenate two strings
+ *
+ * @dest: pointer variable to keep final result
+ * @src: pointer variable to keep the initial string
+ *
+ * Return: final result @dest
+ */
+char *strcat(char *dest, char *src)
+{
+	int str1, str2;
+
+	str1 = 0;
+
+	while (dest[str1])
+	{
+		str++;
+	}
+
+	for (str2 = 0; src[str2]; str2++)
+	{
+		dest[str1++] = src[str2];
+	}
+
+	return (dest);
+}
