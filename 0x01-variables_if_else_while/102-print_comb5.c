8aeae37f9b32620cb38a79ba1f5e583dfc46d7df
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 18:02:01 +0200

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+
+int main(void)
+{
+	int Num1 = 0, Num2;
+
+	while (Num1 <= 99)
+	{
+		seconNum = Num1;
+		while (Num2 <= 99)
+		{
+			if (Num2 != Num1)
+			{
+				putchar((Num1 / 10) + 48);
+				putchar((Num1 % 10) + 48);
+				putchar(' ');
+				putchar((Num2 / 10) + 48);
+				putchar((Num2 % 10) + 48);
+
+				if (Num1 != 98 || Num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			Num2++;
+		}
+		Num1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
