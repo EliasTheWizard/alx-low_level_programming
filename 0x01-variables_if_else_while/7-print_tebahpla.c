dc6092848cccffee79ed02f9c942dce4d4274323
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 17:10:30 +0200

    Mirror on the wall

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point, Print alphabet in reverse
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int asci = 122;
+
+	while (asci >= 97 && asci <= 122)
+	{
+		putchar(asci);
+		asci--;
+	}
+	putchar('\n');
+
+	return (0);
+}
