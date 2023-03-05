bb5d4ade75becd2930166ec54b9ac4ab475c858e
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 17:04:45 +0200

    ASCII Code for Numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point. Print Numbers using
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int numbers = 48;
+
+	while (numbers >= 48 && numbers <= 57)
+	{
+		putchar(numbers);
+	}
+	putcahr('\n');
+
+	return (0);
+}
