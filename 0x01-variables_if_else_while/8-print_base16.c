dfc8caa5c3604877b1f37eaa098017ae3e57452d
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 17:14:18 +0200

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point. Print Hexadecimal Numbers
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int numbers = 48;
+
+	while (numbers >= 48 && numbers <= 57)
+	{
+		putchar(numbers);
+		numbers++;
+	}
+	putchar('\n');
+
+	return (0);
+}
