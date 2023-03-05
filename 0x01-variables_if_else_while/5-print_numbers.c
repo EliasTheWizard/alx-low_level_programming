726a9f06356885da96b2e0907ab033174a1d1d51
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-05 03:50:19 -0800

    Target Threshold

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,12 +7,13 @@
  */
 int main(void)
 {
-  int numbers;
-  
-  for (numbers = 0; numbers <= 9; numbers++)
-  {
-    printf("%d",numbers);
-  }
-  
-  return (0);
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+	{
+		printf("%d", numbers);
+	}
+	printf("\n");
+
+	return (0);
 }
