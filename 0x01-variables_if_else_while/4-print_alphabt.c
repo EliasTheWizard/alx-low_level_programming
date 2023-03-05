4ec49459064596a5a28064dd5b13ae7c7c2cddaa
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 13:11:19 +0200

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point, Print alphabet save 'q' & 'e'
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int asci = 97;
+
+	while (asci >= 97 && asci <= 122)
+	{
+		putchar(asci);
+		asci++;
+	}
+	putchar('\n');
+
+	return (0);
+}
