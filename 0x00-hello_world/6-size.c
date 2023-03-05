8213285f3d441f14eeab23558a9dfc024964ea87
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 17:42:36 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,25 +7,17 @@
  */
 int main(void)
 {
-	int Size_char = sizeof(char);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 
-	int Size_int = sizeof(int);
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 
-	int Size_long = sizeof(long int);
+	printf("Size of an double: %lu byte(s)\n", sizeof(double));
 
-	int Size_Long = sizeof(long long int);
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
 
-	int Size_float = sizeof(float);
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 
-	printf("size of a char: %lu byte(s)\n", Size_char);
-
-	printf("size of an int: %lu byte(s)\n", Size_int);
-
-	printf("size of a long int: %lu byte(s)\n", Size_long);
-
-	printf("size of a long long int: %lu byte(s)\n", Size_Long);
-
-	printf("size of a float: %lu byte(s)\n", Size_float);
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
