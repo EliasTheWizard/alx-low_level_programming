45d99135ef1a4daa4f1baab8c5004e7bf2b2d2c0
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 17:23:52 +0200

    Honey Comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point. Print Numbers in CSV
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int numbers = 48;
+
+	while (numbers >= 48 && numbers <= 57)
+	{
+		putchar(numbers);
+    putchar(',');
+    putchar(' ');
+		numbers++;
+	}
+	putchar('\n');
+
+	return (0);
+}
