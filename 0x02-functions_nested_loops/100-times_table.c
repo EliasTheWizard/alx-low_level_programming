0b7fb801e4d32f131aa0cc0e5aeb953897b60cb9
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 18:14:09 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,46 +10,33 @@
 
 void print_times_table(int n)
 {
-	int product, multiplier, num;
+	int prod, mult, num;
 
 	if (n <= 15 && n >= 0)
 	{
-		for (num = 0; num <= n; num++)
+		for (num = 0; num <= n; ++num)
 		{
 			_putchar(48);
-
-			for (multiplier = 1; multiplier <= n; multiplier++)
+			for (mult = 1; mult <= n; ++mult)
 			{
 				_putchar(',');
-				
 				_putchar(' ');
 
-				product = num * multiplier;
+				prod = num * mult;
 
-				if (product <= 9)
-				{
+				if (prod <= 9)
 					_putchar(' ');
-				}
-				
-				if (product <= 99)
-				{
+				if (prod <= 99)
 					_putchar(' ');
-				}
 
-				if (product >= 100)
+				if (prod >= 100)
 				{
-					_putchar((product / 100) + 48);
-					_putchar((product / 10) % 10 + 48);
-				}
-				else if (product <= 99 && product >= 10)
-				{
-					_putchar((product / 10) + 48);
-					_putchar((product % 10) + 48);
-				}
-				
-				_putchar((product % 10) + 48);
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
-			
 			_putchar('\n');
 		}
 	}
