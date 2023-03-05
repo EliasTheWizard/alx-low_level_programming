eddaaee7b6efdb189135691c6cdb269ef825392a
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-05 12:22:29 +0200

    Save Myself

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,19 @@
 
 /**
  * main - Entry Point, Print alphabet
- * 
+ *
  * Return: 0 success
  */
 int main(void)
 {
 	int asci = 97;
-	
-	while(asci >= 97 && asci <= 122)
+
+	while (asci >= 97 && asci <= 122)
 	{
 		putchar(asci);
 		asci++;
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
