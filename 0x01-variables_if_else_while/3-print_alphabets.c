f5af803936c5842c25ca84952bbd26b5250052a7
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-05 03:05:17 -0800

    Get to the thresold

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+/**
+ * main- Entry Point. Print Alphabets
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int alpha = 97;
+	int ALPHA = 65;
+
+	while (alpha >= 97 && alpha <= 122)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	while (ALPHA >= 65 && ALPHA <= 90)
+	{
+		putchar(ALPHA);
+		ALPHA++;
+	}
+
+	return 0;
+}
