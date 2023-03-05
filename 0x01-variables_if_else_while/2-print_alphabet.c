c9d5cd2e2d54d8ed35aaf5e79051447dcafc23a3
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-05 02:20:02 -0800

    Saving myself

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,20 +2,19 @@
 
 /**
  * main - Entry Point, Print alphabet
- *
+ * 
  * Return: 0 success
  */
-
 int main(void)
 {
-  int asci = 97;
-  
-  while(asci >= 97 && asci <= 122)
-  {
-    putchar(asci);
-    asci++;
-  }
-  putchar('\n');
-  
-  return (0);
+	int asci = 97;
+	
+	while(asci >= 97 && asci <= 122)
+	{
+		putchar(asci);
+		asci++;
+	}
+	putchar('\n');
+	
+	return (0);
 }
