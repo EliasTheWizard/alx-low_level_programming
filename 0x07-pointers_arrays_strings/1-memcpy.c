743250920027928a1180c5d8006780d64502244f
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-06 03:40:48 -0800

    Updatede Memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (map = 0; map < n; map++)
-		dest[map] = src[map];
-
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
 	return (dest);
 }
