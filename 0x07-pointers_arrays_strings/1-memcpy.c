59fa089f99c07575a213adab36657173397819bf
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-06 01:33:06 -0800

    Copy Memory address Code

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _memcpy -Copies Memory Area
+ *
+ * @*dest: Pointer to Variable dest
+ * @*src: pointer to Variable src
+ * @n: Unsigned int variable to store bytes
+ * 
+ * Return: Pointer to @dest
+ */
+char *_memcoy(char *dest, char *src, unsigned int n)
+{
+
