c75a51c02b11988deed5a898e643f667da94094c
EliasTheWizard <mhangomphatso6@gmail.com>
2023-03-06 01:50:31 -0800

    Memory Copy Cat

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@
  * This is a header file to store 
  * Function Prototypes
  */
-char _memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
