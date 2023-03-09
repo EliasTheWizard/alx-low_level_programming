369b6c0d8d0be2a047eeb60fe93d676d5a307264
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-09 18:47:52 +0200

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -40,7 +40,7 @@ int _sqrt_recursion(int n)
 		return (1);
 	else if (n == 0)	/*sqrt(0) == 0*/
 		return (0);
-	else if (n < 0)		/*sqrt(-n) == -1 (NA)*/
+	else if (n <= 0)		/*sqrt(-n) == -1 (NA)*/
 		return (-1);
 	else
 		return (_sqrt_recursion_wrapper(n, 1, n));
