7e7b85f19bb07c62a4e0d8fb5fcd003fefa23da6
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-03-20 20:41:00 +0200

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints the name of the file it was
+ *              compiled from, followed by a new line
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+  /*__FILE__ is a Macro that keeps the name*/
+	printf("%s\n", __FILE__);
+	return (0);
+}
