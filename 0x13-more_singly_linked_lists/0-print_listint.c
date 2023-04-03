3d254a196f0639a79ecb055d967505f10dc0dcf3
Error_255 <86038081+EliasTheWizard@users.noreply.github.com>
2023-04-03 15:14:56 +0200

    Create 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,30 @@
+/**
+ * print_listint - a function that prints all elements of a list
+ *
+ * @h: pointer to first node
+ *
+ * Return: the number of nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	size_t node_count = 1;
+
+	/* If h is null return 0 as value of pointer to first Node*/
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+
+		/*go to the next node*/
+		h = h->next;
+		/*count no of nodes*/
+		node_count++;
+	}
+
+	/*print last node*/
+	printf("%d\n", h->n);
+
+	return (node_count);
+}
